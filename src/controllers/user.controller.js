dedd8776a597b77758bc2c0cb265044c88f0204b
shailendrasdl <shailendrakumarsen@gmail.com>
2024-01-06 01:22:43 +0530

    Implement updateAccount & updateAvatar API

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -183,7 +183,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
   if (!(newPassword === confPassword)) {
     throw new ApiError(400, "newPassword & confPassword do not match");
   }
-  const user = await User.findById(req.user?.id);
+  const user = await User.findById(req.user?._id);
   const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
 
   if (!isPasswordCorrect) {
@@ -203,7 +203,44 @@ const getCurrentUser = asyncHandler(async (req, res) => {
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
-  
+  const { fullName, email } = req.body;
+
+  if (!fullName || !email) {
+    throw new ApiError(400, "All fields are required");
+  }
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName,
+        email,
+      },
+    },
+    { new: true }
+  ).select("-password");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"));
+});
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req.file?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is missing ");
+  }
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  if (!avatar.url) {
+    throw new ApiError(400, "Error while uploading on avatar");
+  }
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: { avatar: avatar.url },
+    },
+    { new: true }
+  ).select("-password");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Avatar updated successfully"));
 });
 
 export {
@@ -211,6 +248,8 @@ export {
   logoutUser,
   registerUser,
   getCurrentUser,
+  updateUserAvatar,
   refereshAccessToken,
+  updateAccountDetails,
   changeCurrentPassword,
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,6 +8,8 @@ import {
   logoutUser,
   refereshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -35,5 +37,9 @@ router.route("/refresh-token").post(refereshAccessToken);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 
 export default router;
