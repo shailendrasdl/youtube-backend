1d33901d6a872ec30b248f90c6b60c35c4d1f678
shailendrasdl <shailendrakumarsen@gmail.com>
2024-01-09 19:00:43 +0530

    add like,comment, tweet,subscription,video Routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 
 import userRouter from "./routes/user.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 const app = express();
 
@@ -21,5 +27,11 @@ app.use(cookieParser());
 app.use(helmet());
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 export { app };
---
+++ src/controllers/video.controller.js
@@ -0,0 +1,4 @@
+import mongoose from "mongoose";
+import { asyncHandler } from "../utils/asyncHandler";
+
+const getAllVideos = asyncHandler(async (req, res) => {});
