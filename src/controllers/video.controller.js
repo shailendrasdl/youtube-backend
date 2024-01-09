715e3048d1d11d0b1e15b19cb67372f5f7e0445c
shailendrasdl <shailendrakumarsen@gmail.com>
2024-01-09 20:01:22 +0530

    add getAllVideos API

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,4 +1,11 @@
-import mongoose from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler";
+import { Video } from "../models/video.model";
+import { ApiResponse } from "../utils/ApiResponse";
 
-const getAllVideos = asyncHandler(async (req, res) => {});
+const getAllVideos = asyncHandler(async (req, res) => {
+  const video = await Video.find();
+  console.log("video :", video);
+  return res.status(200).json(new ApiResponse(200, video, "All Videos"));
+});
+
+export { getAllVideos };
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+//import { getAllVideos, publishVideo } from "../controllers/video.controller";
 
 const router = Router();
 
 
+//router.route("/getAllVideos").get(getAllVideos);
+//router.route("/publishVideo").post
 
 export default router;
