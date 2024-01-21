f1536629d23e2c852a9d21648f18df7f08075588
shailendrasdl <shailendrakumarsen@gmail.com>
2024-01-21 21:15:14 +0530

    add dashboard Router

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
