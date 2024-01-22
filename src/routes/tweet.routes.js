489d9e9d583e29a62b044391a47e941eba526a42
shailendrasdl <shailendrakumarsen@gmail.com>
2024-01-22 15:54:07 +0530

    Add createTweet, updateTweet, deleteTweet, getUserTweets API

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,5 +1,19 @@
 import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import {
+  createTweet,
+  deleteTweet,
+  getUserTweets,
+  updateTweet,
+} from "../controllers/tweet.controller.js";
 
 const router = Router();
+router.use(verifyJWT, upload.none());
+
+router.route("/").post(createTweet);
+router.route("/:tweetId").patch(updateTweet);
+router.route("/:tweetId").patch(deleteTweet);
+router.route("/user/:userId").get(getUserTweets);
 
 export default router;
