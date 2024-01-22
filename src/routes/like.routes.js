ea2b969ba4d81c95137178094e1836c9764c7fd9
shailendrasdl <shailendrakumarsen@gmail.com>
2024-01-22 12:21:29 +0530

    add toggleVideoLike, toggleCommentLike, toggleTweetLike, getLikedVideos

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
-import { toggleVideoLike } from "../controllers/like.controller.js";
+import {
+  getLikedVideos,
+  toggleCommentLike,
+  toggleTweetLike,
+  toggleVideoLike,
+} from "../controllers/like.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 // Apply verifyJWT middleware to all routes in this file
-//router.use(verifyJWT);
+router.use(verifyJWT);
 
-router.route("/toggle/v/:videoId").post(toggleVideoLike);
+router.route("/videos").get(getLikedVideos);
+router.route("/toggle/video/:videoId").post(toggleVideoLike);
+router.route("/toggle/tweet/:tweetId").post(toggleTweetLike);
+router.route("/toggle/comment/:commentId").post(toggleCommentLike);
 
 export default router;
